5140d1b3a60986d799a492bf8359da24564359dc
Lukasz Kaminski <kukaszlaminski@gmail.com>
2019-05-29 09:36:55 +0200

    add funcionality

---
+++ main.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+const h1 = document.querySelector('h1');
+let image = true;
+let title = true;
+
+const changeImage = () => {
+ image = !image;
+ title = !title;
+ header.style.backgroundImage = image ? "url(drink.jpg)" : "url(bulding)";
+ h1.textContent = title ? "Śniadanie" : "Coś fajnego";
+
+ header.addEventListener("click", changeImage)
+}
